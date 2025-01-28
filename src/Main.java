049fbeac4a0724c77075264cebaf438eb74f323e
Redstone69 <89121962+Redstone69@users.noreply.github.com>
2025-01-28 13:23:59 -0500

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,8 @@ public class Main {
         double[] prec = {95, 60, 25, 5, 0, 75, 90};
         String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
 
-        // Loop through the temperatures and precipitation data
         for (int count = 0; count < temps.length; count++) {
             if (temps[count] <= 32 && prec[count] > 50) {
-                // Print the day if conditions are met
                 System.out.println(days[count] + " is likely to snow.");
             }
         }
